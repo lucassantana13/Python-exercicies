5d0d06d7b0727264d4a0e406e8edb8215d446571
Lucas Santana <lucas.sidney@hotmail.com>
2023-03-17 00:42:22 -0300

    att

--- Horaatual.py
+++
@@ -1,4 +0,0 @@
-import datetime
-
-hora_atual = datetime.datetime.now()
-print(hora_atual)
--- areacirc.py
+++
@@ -1,3 +0,0 @@
-r1 = float(input('Insira o valor do raio do circulo: '))
-area = (r1**2)*3.141592
-print(f'O valor da área é: {area:.2f}')
